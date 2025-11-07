af45ca79b44472137000bf9310a5355d4d8844a4
Sebastián López <sebalozu28@gmail.com>
2025-11-07 15:30:30 -0300

    cambio nombre pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Acreditaciones Santiago 2025",
+  title: "Hockey women's junior world cup 2025",
   description: "Página para acreditaciones del mundial de hockey",
 };
 
